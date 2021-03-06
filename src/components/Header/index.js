db1b6ce205ac64ce4147ff488be26704a8288cb4
Felipe Silva <carlosfelipesilva.fs@gmail.com>
2021-03-06 20:33:40 -0300

    Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,23 @@
+import { Container, Title, Cart } from './styles';
+import { BiCartAlt } from 'react-icons/bi';
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/CartContext';
+import { MdGames } from 'react-icons/md';
+
+export function Header() {
+
+  const { cartItems } = useContext(CartContext);
+
+  return (
+      <Container>
+        <Title to="/"><i>E-commerce Games</i><div><MdGames size={40} /></div></Title>
+        <Cart to="/cart">
+          <div>
+            <strong>Meu carrinho</strong>
+            <span>{cartItems.length} itens</span>
+          </div>
+        <BiCartAlt size={56} color="whitesmoke"></BiCartAlt>
+        </Cart>
+      </Container>
+  );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,47 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 50px 0%;
+`;
+
+export const Title = styled(Link)`
+  font-size: 32px;
+  font-weight: 700;
+  color: whitesmoke;
+  text-decoration: none;
+  display: flex;
+
+  svg {
+    margin-left: .3rem;
+  }
+
+`;
+
+export const Cart = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  transition: opacity 0.2s;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  div {
+    text-align: right;
+    margin-right: 10px;
+  }
+  strong {
+    display: block;
+    color: #fff;
+  }
+  span {
+    font-size: 12px;
+    color: #999;
+  }
+`;
